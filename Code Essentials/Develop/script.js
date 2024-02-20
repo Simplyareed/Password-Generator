6c8bd8132b644586d5db921393a9c8054ed843cb
Simplyareed <adam.reed1991@gmail.com>
2024-02-19 20:10:30 -0800

    Updated Java, addded functions,alerts,comments

--- Code Essentials/Develop/script.js
+++ Code Essentials/Develop/script.js
@@ -1,4 +1,26 @@
 // Assignment code here
+//Added function //
+function generatePassword() {
+  let lengthString = prompt("Enter the length of the password (between 8 and 128 characters):");
+  let includeLowercase = confirm("Do you want to include lowercase?");
+
+//Added Alerts w/strings for length//
+  if(lengthString < 8 ) {
+    alert("Password must be at least 8 digits long")
+    return
+  }
+  if(lengthString > 128) {
+    alert("Password must be no longer than 128 digits")
+    return
+  }
+}
+
+
+  //  // String lower_cases = "acbdefghijklmnopqrstuvwxyz";
+  //   String upper_cases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   String special_characters = "!@#$%^&*()_+{}[];':,.<>/?\";
+  //   String Digits = "0123456789";//
+
 
 
 // Get references to the #generate element
