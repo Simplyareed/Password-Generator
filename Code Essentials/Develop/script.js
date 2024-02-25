eee3116e895582fd18f0a76128ad4886eccc2233
Simplyareed <adam.reed1991@gmail.com>
2024-02-25 15:58:27 -0800

    Added Booleans, comments, console logs

--- Code Essentials/Develop/script.js
+++ Code Essentials/Develop/script.js
@@ -1,36 +1,51 @@
 // Assignment code here
+   let includeLowerCase = "acbdefghijklmnopqrstuvwxyz";
+   let includeUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   let includeSpecialChar = "!@#$%^&*()_+{}[];':,.<>/?";
+   let includeNumeric = "0123456789";
+   
+   
+
+
 //Added function //
 function generatePassword() {
+  let includeLowerCase = confirm("Do you want to include lowercase?");
+  let includeUpperCase = confirm("Do you want to include uppercase?");
+  let includeNumeric = confirm("Do you want to include numerical numbers?");
+  let includeSpecialChar = confirm("Do you want to include special characters?");
   let lengthString = prompt("Enter the length of the password (between 8 and 128 characters):");
-  let includeLowercase = confirm("Do you want to include lowercase?");
+  
 
 //Added Alerts w/strings for length//
-  if(lengthString < 8 ) {
-    alert("Password must be at least 8 digits long")
-    return
+// Added booleans for alert statements 
+  if(lengthString < 8) {
+    alert("Password must be at least 8 digits long");
+    return;
   }
   if(lengthString > 128) {
-    alert("Password must be no longer than 128 digits")
-    return
+    alert("Password must be no longer than 128 digits");
+    return;
   }
+if (includeLowerCase || includeUpperCase ||includeNumeric ||includeSpecialChar) {
+  alert("Password has succesfully been generated!");
+
+} else {
+  alert("Password must meet at least one requirement: Must include a lowercase, uppercase, numerical number, or special characters.");
 }
-
-
-  //  // String lower_cases = "acbdefghijklmnopqrstuvwxyz";
-  //   String upper_cases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  //   String special_characters = "!@#$%^&*()_+{}[];':,.<>/?\";
-  //   String Digits = "0123456789";//
-
+}
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+console.log("THIS IS MY BUTTON" + generateBtn)
 
 // Write password to the #password input
+// Added console log to see where the variables were tied to
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+   alert('Generated Password: ' , password);
+console.log("THIS IS MY PW" + password);
   passwordText.value = password;
 
 }
